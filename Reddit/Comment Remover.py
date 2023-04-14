a218662b0421c9a6073f6c31e0bb6aef9116aa4e
bizbazboz <66413191+bizbazboz@users.noreply.github.com>
2023-04-14 17:32:31 +0100

    Update Comment Remover.py

--- Reddit/Comment Remover.py
+++ Reddit/Comment Remover.py
@@ -10,7 +10,7 @@ time.sleep(2)
 tk.messagebox.showwarning('Alert', """Set up a Script at the bottom of this page, and get your information to input into the form in the script!""")
 reddit = praw.Reddit(client_id=input("Client ID: "), client_secret=input("Client Secret: "), user_agent='kjhgfgbhjifdnbc vghnjfd', username=input("Username: "), password=input("Password: "))
 excluded_subs = []
-for comment in reddit.user.me().comments.new(limit=None):
-    comment.delete()
-    print(f"Comment deleted! - {comment}")
-print("Done")
+while True:
+    for comment in reddit.user.me().comments.new(limit=None):
+        comment.delete()
+        print(f"Comment deleted! - {comment}")
